842256afa131724baacbb812aca5b7076f51525f
AQUA651 <86642639+NoriStock1983@users.noreply.github.com>
2025-09-01 21:09:18 +0900

    add embedding

--- main.py
+++ main.py
@@ -1,2 +1,17 @@
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from dotenv import load_dotenv
+
+# .envファイルから環境変数を読み込む
+load_dotenv()
+
 def main():
-    print("HelloWorld")
+    # ChatGoogleGenerativeAIクラスをインスタンス化します
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/text-embedding-004")
+    prompt = "技術記事のタイトルをよしなに生成してくがるAIの名称を考えてください。"
+    embedding = embeddings.embed_documents([prompt])
+    print(embedding)
+
+if __name__ == "__main__":
+    main()
